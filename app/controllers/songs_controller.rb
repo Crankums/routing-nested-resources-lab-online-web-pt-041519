68e0e6b0400d53e9b0d6aded6141ca0205a05f95
Craig Donavin <crdonavin@gmail.com>
2019-09-26 21:33:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,13 @@ class SongsController < ApplicationController
   end
 
   def show
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        redirect_to artist_songs_path(@artist)
+      end
+    else
     @song = Song.find(params[:id])
   end
 
